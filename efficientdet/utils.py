e228bc24b395b6e64f98247db9ff576b4b3a1aaf
anslt <ge75wed@mytum.de>
2020-07-04 17:57:06 +0200

    show annotation

--- efficientdet/utils.py
+++ efficientdet/utils.py
@@ -160,6 +160,8 @@ class Polygons(object):
         self.mode = mode
 
     def transpose(self, method):
+        if self.polygons == []:
+            return Polygons([], size=self.size, mode=self.mode)
         if method not in (FLIP_LEFT_RIGHT, FLIP_TOP_BOTTOM):
             raise NotImplementedError(
                 "Only FLIP_LEFT_RIGHT and FLIP_TOP_BOTTOM implemented"
@@ -179,13 +181,14 @@ class Polygons(object):
             TO_REMOVE = 1
             try:
                 p[idx::2] = dim - poly[idx::2] - TO_REMOVE
-            except IndexError:
-                print("poly", poly)
             flipped_polygons.append(p)
 
         return Polygons(flipped_polygons, size=self.size, mode=self.mode)
 
     def crop(self, box):
+        if self.polygons == []:
+            return Polygons([], size=self.size, mode=self.mode)
+        
         w, h = box[2] - box[0], box[3] - box[1]
 
         # TODO chck if necessary
@@ -202,6 +205,8 @@ class Polygons(object):
         return Polygons(cropped_polygons, size=(w, h), mode=self.mode)
 
     def resize(self, size, *args, **kwargs):
+        if self.polygons == []:
+            return Polygons([], size=self.size, mode=self.mode)
         ratios = tuple(float(s) / float(s_orig) for s, s_orig in zip(size, self.size))
         if ratios[0] == ratios[1]:
             ratio = ratios[0]
@@ -219,6 +224,8 @@ class Polygons(object):
         return Polygons(scaled_polygons, size=size, mode=self.mode)
 
     def convert(self, mode):
+        if self.polygons == []:
+            return Polygons([], size=self.size, mode=self.mode)
         width, height = self.size
         if mode == "mask":
             rles = mask_utils.frPyObjects(
