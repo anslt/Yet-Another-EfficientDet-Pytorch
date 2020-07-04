120d0239bac3ac6dbc09f5b37537e3e2897757db
anslt <ge75wed@mytum.de>
2020-07-04 19:43:11 +0200

    resize mask size

--- utils/utils.py
+++ utils/utils.py
@@ -365,8 +365,8 @@ def to_bbox_targets(annotations, masks, img_size=512):
     for annot, mask in zip(annotations, masks):
         bboxes = annot[:, :4]
         labels = annot[:, -1]
-        print(len(bboxes))
-        print(len(mask.polygons))
+        print("annot",len(bboxes))
+        print("mask",len(mask.polygons))
         boxlist = BoxList(bboxes.to(device), (img_size, img_size))
         boxlist.add_field("labels", labels.type(torch.LongTensor).to(device))
         boxlist.add_field("masks", mask.to(device))
