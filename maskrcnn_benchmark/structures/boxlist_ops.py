d9e2d612676634f1f5c3f51ed65125a13b80d8d5
anslt <ge75wed@mytum.de>
2020-07-04 20:59:52 +0200

    fix cuda problem

--- maskrcnn_benchmark/structures/boxlist_ops.py
+++ maskrcnn_benchmark/structures/boxlist_ops.py
@@ -94,8 +94,6 @@ def _cat(tensors, dim=0):
     Efficient version of torch.cat that avoids a copy if there is only a single element in a list
     """
     assert isinstance(tensors, (list, tuple))
-    for k in tensors:
-        print(k.device)
     if len(tensors) == 1:
         return tensors[0]
     return torch.cat(tensors, dim)
